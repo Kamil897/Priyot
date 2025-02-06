4287a2029c8a45b1e2664a775f33ea71c42ef5f9
Kamil897 <kamil2007shakirov@gmail.com>
2025-02-06 19:36:00 +0500

    че

--- src/components/MusicPlayer/MusicPlayer.jsx
+++ src/components/MusicPlayer/MusicPlayer.jsx
@@ -6,90 +6,69 @@ const tracks = [
   {
     name: "Stan",
     artist: "Eminem, Dido",
-    cover: "/images/Stan.jpg",
+    cover: process.env.PUBLIC_URL +  "/images/Stan.jpg",
     source: process.env.PUBLIC_URL +  "/music/Stan.mp3",
-    url: "https://youtu.be/nGcM8afe0F4",
-    favorited: false,
   },
   {
     name: "She Said Shes From The Islands",
     artist: "Tomo Frozy",
-    cover: "/images/Island.jpg",
+    cover: process.env.PUBLIC_URL +  "/images/Island.jpg",
     source: process.env.PUBLIC_URL +  "/music/She_Said_Shes_From_The_Islands.mp3",
-    url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
-    favorited: false,
   },
   {
     name: "Promise to Myself",
     artist: "Matt Heath",
-    cover: "/images/Myself.jpg",
+    cover: process.env.PUBLIC_URL +  "/images/Myself.jpg",
     source: process.env.PUBLIC_URL +  "/music/Matt_Heath.mp3",
-    url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
-    favorited: false,
   },
   {
     name: "Just The Two Of Us ",
     artist: "Grover Washington, Jr.",
-    cover: "/images/Grover_Washington.jpg",
+    cover: process.env.PUBLIC_URL +  "/images/Grover_Washington.jpg",
     source: process.env.PUBLIC_URL +  "/music/Grover_Washington.mp3",
-    url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
-    favorited: false,
+
   },
   {
     name: "Headlock (Immis Radio Mix)",
     artist: "Imogen Heap",
-    cover: "/images/Imogen_Heap.jpg",
+    cover: process.env.PUBLIC_URL +  "/images/Imogen_Heap.jpg",
     source: process.env.PUBLIC_URL +  "/music/Imogen-Heap.mp3",
-    url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
-    favorited: false,
   },
   {
     name: "Je Reve",
     artist: "La Meprise",
-    cover: "/images/Je_Reve.jpg",
+    cover: process.env.PUBLIC_URL +  "/images/Je_Reve.jpg",
     source: process.env.PUBLIC_URL +  "/music/Je_Reve.mp3",
-    url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
-    favorited: false,
   },
   {
     name: "Die With A Smile",
     artist: "Lady Gaga & Bruno Mars",
-    cover: "/images/LadyGaga_BrunoMars_.jpg",
+    cover: process.env.PUBLIC_URL +  "/images/LadyGaga_BrunoMars_.jpg",
     source: process.env.PUBLIC_URL +  "/music/Lady_Gaga_Bruno_Mars.mp3",
-    url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
-    favorited: false,
   },
   {
     name: "Одно и тоже",
     artist: "iowa",
-    cover: "/images/odno.jpg",
+    cover: process.env.PUBLIC_URL +  "/images/odno.jpg",
     source: process.env.PUBLIC_URL +  "/music/odno_i_toje.mp3",
-    url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
-    favorited: false,
   },
   {
     name: "90",
     artist: "Pompeya",
-    cover: "/images/Hotel.jpg",
+    cover: process.env.PUBLIC_URL +  "/images/Hotel.jpg",
     source: process.env.PUBLIC_URL +  "/music/Pompeya.mp3",
-    url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
-    favorited: false,
   },
   {
     name: "Like Him (feat. Lola Young)",
     artist: "Tyler, The Creator CHROMAKOPIA",
-    cover: "/images/Tyler.jpg",
+    cover: process.env.PUBLIC_URL +  "/images/Tyler.jpg",
     source: process.env.PUBLIC_URL +  "/music/Tyler.mp3",
-    url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
-    favorited: false,
   },
   {
     name: "Fly me to the moon Squid game",
     artist: "Joo Won",
-    cover: "/images/Squid.jpg",
+    cover: process.env.PUBLIC_URL +  "/images/Squid.jpg",
     source: process.env.PUBLIC_URL +  "/music/Squid_game.mp3",
-    url: "https://www.youtube.com/watch?v=ICjyAe9S54c",
-    favorited: false,
   },
 ];
 
@@ -101,29 +80,29 @@ const formatTime = (time) => {
   return `${minutes < 10 ? "0" : ""}${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 };
 
-const MusicPlayer = ({ isFixed = false }) => {
-  const location = useLocation();
+const MusicPlayer = () => {
   const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [barWidth, setBarWidth] = useState("0%");
+  const [progress, setProgress] = useState(0);
   const [duration, setDuration] = useState("00:00");
   const [currentTime, setCurrentTime] = useState("00:00");
-  const audioRef = useRef(null);
+  const audioRef = useRef(new Audio(tracks[currentTrackIndex].source));
 
   useEffect(() => {
-    if (!audioRef.current) return;
     const audio = audioRef.current;
     audio.src = tracks[currentTrackIndex].source;
-    audio.load();
 
-    const updateMetadata = () => setDuration(formatTime(audio.duration));
+    const updateMetadata = () => {
+      setDuration(formatTime(audio.duration));
+    };
+
     const updateProgress = () => {
-      setBarWidth(`${(audio.currentTime / audio.duration) * 100}%`);
+      setProgress((audio.currentTime / audio.duration) * 100);
       setCurrentTime(formatTime(audio.currentTime));
     };
+
     const handleTrackEnd = () => {
       nextTrack();
-      setIsPlaying(true);
     };
 
     audio.addEventListener("loadedmetadata", updateMetadata);
@@ -138,66 +117,65 @@ const MusicPlayer = ({ isFixed = false }) => {
   }, [currentTrackIndex]);
 
   const playPause = () => {
-    if (!audioRef.current) return;
+    const audio = audioRef.current;
     if (isPlaying) {
-      audioRef.current.pause();
+      audio.pause();
     } else {
-      audioRef.current.play().catch((error) => {
-        console.error("Ошибка воспроизведения:", error);
-        alert("Ошибка воспроизведения. Возможно, формат файла не поддерживается.");
-      });
+      audio
+        .play()
+        .catch((error) => console.error("Ошибка воспроизведения:", error));
     }
     setIsPlaying(!isPlaying);
   };
 
-  const nextTrack = () => setCurrentTrackIndex((prev) => (prev + 1) % tracks.length);
-  const prevTrack = () => setCurrentTrackIndex((prev) => (prev - 1 + tracks.length) % tracks.length);
+  const nextTrack = () => {
+    setCurrentTrackIndex((prevIndex) => (prevIndex + 1) % tracks.length);
+    setIsPlaying(false);
+  };
+
+  const prevTrack = () => {
+    setCurrentTrackIndex((prevIndex) =>
+      prevIndex - 1 < 0 ? tracks.length - 1 : prevIndex - 1
+    );
+    setIsPlaying(false);
+  };
+
+  const seek = (event) => {
+    const rect = event.target.getBoundingClientRect();
+    const percentage = (event.clientX - rect.left) / rect.width;
+    const audio = audioRef.current;
+    audio.currentTime = percentage * audio.duration;
+  };
 
   return (
-    <div className={`player-music ${isFixed ? "player--fixed" : ""}`}>
-      <audio key={currentTrackIndex} ref={audioRef} preload="auto" />
+    <div className="music-player">
+      <audio ref={audioRef} />
 
       <div className="player-cover">
-        <img src={tracks[currentTrackIndex].cover} alt={tracks[currentTrackIndex].name} className="player-cover__img" />
+        <img
+          src={tracks[currentTrackIndex].cover}
+          alt={tracks[currentTrackIndex].name}
+          className="player-cover__img"
+        />
       </div>
 
       <div className="album-info">
-        {isFixed ? (
-          <p className="album-info__playing-now">Сейчас играет: {tracks[currentTrackIndex].name} - {tracks[currentTrackIndex].artist}</p>
-        ) : (
-          <>
-            <h2 className="album-info__name">{tracks[currentTrackIndex].name}</h2>
-            <p className="album-info__track">{tracks[currentTrackIndex].artist}</p>
-          </>
-        )}
+        <h2 className="album-info__name">{tracks[currentTrackIndex].name}</h2>
+        <p className="album-info__artist">{tracks[currentTrackIndex].artist}</p>
       </div>
 
-      <div className={`progress ${isFixed ? "progress_none" : ""}`}>
-        <div
-          className="progress__bar"
-          onClick={(e) => {
-            const rect = e.target.getBoundingClientRect();
-            const percentage = (e.clientX - rect.left) / rect.width;
-            audioRef.current.currentTime = percentage * audioRef.current.duration;
-          }}
-        >
-          <div className="progress__current" style={{ width: barWidth }}></div>
-        </div>
-        <div className="progress__time">
-          <span>{currentTime}</span> / <span>{duration}</span>
-        </div>
+      <div className="progress-bar" onClick={seek}>
+        <div className="progress" style={{ width: `${progress}%` }}></div>
+      </div>
+      <div className="time-info">
+        <span>{currentTime}</span> / <span>{duration}</span>
       </div>
 
       <div className="player-controls">
         <button className="player-controls__item" onClick={prevTrack}>
-          <img src="./Shape2-removebg-preview.png" alt="Previous" className="player-controls__icon" />
-        </button>
-        <button className="player-controls__item -xl" onClick={playPause}>
-          {isPlaying ? "⏸️" : "▶️"}
-        </button>
-        <button className="player-controls__item" onClick={nextTrack}>
-          <img src="./Shape.png" alt="Next" className="player-controls__icon" />
-        </button>
+            <img src="./Shape2-removebg-preview.png" alt="Previous" className="player-controls__icon" />
+          </button>
+          <button onClick={playPause}>{isPlaying ? "⏸️" : "▶️"}</button>
       </div>
     </div>
   );
