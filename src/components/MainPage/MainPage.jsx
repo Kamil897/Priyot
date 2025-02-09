a373b93a2acc7e77299e2e9b878b4b50eeb96920
Kamil897 <kamil2007shakirov@gmail.com>
2025-02-09 15:40:45 +0500

    88

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -2,23 +2,30 @@ import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import MusicPlayer from "../MusicPlayer/MusicPlayer";
 import s from "./MainPage.module.scss";
-import Games from "../Games/Games";
+import Games from "../Games/Games"; // Удалил .jsx, т.к. Webpack сам это обрабатывает
 import WeatherWidget from "../WeatherWidget/WeatherWidget";
+import defaultAvatar from "../../assets/profileimg.png"; // Импорт изображения, если оно хранится в src
 
 const MainPage = () => {
   const [userData, setUserData] = useState(null);
   const navigate = useNavigate();
 
+  // Проверяем наличие пользователя в локальном хранилище
   useEffect(() => {
     const username = localStorage.getItem("loggedInUsername");
     if (!username) {
       navigate("/login");
     } else {
-      const user = JSON.parse(localStorage.getItem(username));
-      setUserData(user);
+      const user = JSON.parse(localStorage.getItem(username) || "{}");
+      if (Object.keys(user).length === 0) {
+        navigate("/login");
+      } else {
+        setUserData(user);
+      }
     }
   }, [navigate]);
 
+  // Обработчик выхода из аккаунта
   const handleLogout = () => {
     localStorage.removeItem("loggedInUsername");
     navigate("/login");
@@ -32,7 +39,7 @@ const MainPage = () => {
             <div className={s.profile}>
               <img
                 className={userData.avatar ? s.pfp : s.defaultPfp}
-                src={userData.avatar || "profileimg.png"}
+                src={userData.avatar || defaultAvatar}
                 alt="Profile"
               />
               <h2 className={s.username}>
@@ -72,7 +79,7 @@ const MainPage = () => {
           <p>Загрузка...</p>
         )}
       </div>
-      
+
       <div className={s.other}>
         <div className={s.playerSection}>
           <MusicPlayer isFixed={false} />
@@ -82,9 +89,6 @@ const MainPage = () => {
           <WeatherWidget />
         </div>
       </div>
-
-
-
     </div>
   );
 };
