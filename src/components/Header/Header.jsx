86227cd4824d293198af248e158f4adc58d9ae4b
Kamil897 <kamil2007shakirov@gmail.com>
2025-02-09 15:54:14 +0500

    77

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -111,10 +111,46 @@ const Header = () => {
             </div>
 
             <div className={s.changeTheme}>
-              <button onClick={toggleTheme} className={s.button}>
-                {isDarkMode ? '☀️' : '🌑'}
-              </button>
-            </div>
+  <button onClick={toggleTheme} className={s.button}>
+    {isDarkMode ? (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="36"
+        height="36"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={s.icon}
+      >
+        <path d="M21.257 10.962c.474.62.474 1.457 0 2.076C19.764 14.987 16.182 19 12 19c-4.182 0-7.764-4.013-9.257-5.962a1.692 1.692 0 0 1 0-2.076C4.236 9.013 7.818 5 12 5c4.182 0 7.764 4.013 9.257 5.962z" />
+        <circle cx="12" cy="12" r="3" />
+      </svg>
+    ) : (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="36"
+        height="36"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className={s.icon}
+      >
+        <path d="M2 10s3.5 4 10 4 10-4 10-4" />
+        <path d="M4 11.645L2 14" />
+        <path d="M22 14l-1.996-2.352" />
+        <path d="M8.914 13.68L8 16.5" />
+        <path d="M15.063 13.688L16 16.5" />
+      </svg>
+    )}
+  </button>
+</div>
+
           </nav>
         </div>
       </header>
