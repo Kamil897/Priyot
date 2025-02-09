2925289961c1d02f003b136b66c087579ab1e1f2
Kamil897 <kamil2007shakirov@gmail.com>
2025-02-09 14:50:10 +0500

    554

--- src/pages/magaz.jsx
+++ src/pages/magaz.jsx
@@ -15,7 +15,7 @@ const prefixes = [
     id: 2, 
     name: "Сухой корм для собак", 
     description: "Высококачественный корм для собак всех пород.", 
-    image: "https://images.uzum.uz/cqosj8ssslomdvnjn6cg/t_product_540_high.jpg",
+    image: "https://api.zapovednik96.ru/upload/iblock/7c0/7c003b7dc5f4c9b095e3027060846256.jpg",
     price: 1500 
   },
   { 
@@ -85,7 +85,7 @@ const prefixes = [
     id: 12,
     name: "Лазерная указка для кошек",
     description: "Веселая и активная игра с лазерной точкой.",
-    image: "https://images.uzum.uz/cngsmhku2hhlb05g6nfg/original.jpg",
+    image: "https://avatars.mds.yandex.net/i?id=80918d15f6453128721378ac9037b54e_l-10599899-images-thumbs&n=13",
     price: 450
   }
 ];
